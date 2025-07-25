0f64b83156c389620692f82dc3962856720cb99a
Moussa Daoud <moussa-abdillahi.daoud@polymtl.ca>
2025-07-24 20:36:13 -0400

    audit module

---
+++ backend/src/audit/audit.module.ts
@@ -0,0 +1,16 @@
+import { Module, Global } from '@nestjs/common';
+import { AuditService } from '../common/services/audit.service';
+import { AuditInterceptor } from '../common/interceptors/audit.interceptor';
+
+@Global() // Make AuditService available everywhere
+@Module({
+    providers: [
+        AuditService,
+        AuditInterceptor,
+    ],
+    exports: [
+        AuditService,
+        AuditInterceptor,
+    ],
+})
+export class AuditModule { }
